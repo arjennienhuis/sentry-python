9d1a55b9329623f52dd65ed3aff1e909a5f17120
Daniel Szoke <daniel.szoke@sentry.io>
2024-04-25 11:54:53 +0200

    feat(celery): Set task ID on span
    Closes #2974

--- sentry_sdk/integrations/celery/__init__.py
+++ sentry_sdk/integrations/celery/__init__.py
@@ -337,6 +337,7 @@ def _wrap_task_call(task, f):
                 op=OP.QUEUE_PROCESS, description=task.name
             ) as span:
                 _set_messaging_destination_name(task, span)
+                span.set_data("messaging.message.id", task.request.id)
                 with capture_internal_exceptions():
                     span.set_data("messaging.message.retry.count", task.request.retries)
                 return f(*args, **kwargs)
